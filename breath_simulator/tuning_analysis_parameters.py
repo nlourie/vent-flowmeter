fe0cb2e69d141d8e494f30a086fb8a1d99c68b34
nlourie <nlourie@sas.upenn.edu>
2020-05-26 10:55:20 -0400

    added mvm

--- breath_simulator/tuning_analysis_parameters.py
+++ breath_simulator/tuning_analysis_parameters.py
@@ -62,6 +62,32 @@ def breath_detect_coarse(flow,fs,plotflag = False):
     print('found peaks at index = ',peak_index)
     return peak_index
 
+def zerophase_lowpass(x,lf,fs):
+    # filter flow using S-G filter
+    
+    # filter flow
+    N = 2 # the higher the sharper the peak is
+    # length = 1s (cut off frequency = 0.965 Hz)
+    # Refer. Schafer, 2011, What is S-G filter.
+    # fc_n = (N+1)/(3.2M - 4.6) (normalized unit)
+    # fc(Hz) = fc_n*fs/2;
+    # N: order: M length;
+    # fc=lf(Hz) -> fc_n = 2*lf/fs;
+    # M = [(N+1)*fs/(2*lf)+4.6]/3.2;
+    
+    # l_lfilter = round(fs*lf); % the longer, the smoother
+    # l_lfilter = round(fs*lf); % the longer, the smoother
+    
+    l_lfilter = np.int(np.round(((N+1)*fs/(2*lf)+4.6)/3.2))
+    #print('l_lfilter = ',l_lfilter)
+    if np.mod(l_lfilter,2) == 0:
+        l_lfilter = l_lfilter + 1
+        #print('made odd: l_lfilter = ',l_lfilter)
+    
+    # filter flow signal
+    x_filt = signal.savgol_filter(x,polyorder = N,window_length = l_lfilter)
+    return x_filt
+
 
 # import the data
 
@@ -92,12 +118,14 @@ elif model == 'spline':
 
 vol_corr = vol - v_drift
 
+flow_filt = zerophase_lowpass(flow,0.5,fs)
 
 
 plt.figure(figsize = (10,10))
 
 plt.subplot(3,1,1)
-plt.plot(time,flow)
+plt.plot(time,flow,label = 'flow')
+plt.plot(time,flow_filt,label = 'filtered flow')
 plt.ylabel('Flow')
 
 plt.subplot(3,1,2)
